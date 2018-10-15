eb18944d8d8d6b8876ad0731789a847867be88ba
Mayank <mayank.oli@mountblue.io>
2018-10-15 11:25:28 +0530

    hshs

--- hospital_automation/urls.py
+++ hospital_automation/urls.py
@@ -2,3 +2,7 @@ from django.urls import path
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.views import LogoutView
 from . import views
+
+urlpatterns = [
+    path('doctors',views.doctors, name='doctors'),
+]
--- hospital_automation/views.py
+++ hospital_automation/views.py
@@ -1,11 +1,2 @@
 from django.shortcuts import render
 
-<<<<<<< HEAD
-def doctors():
-    pass
-
-def index():
-=======
-def reception():
->>>>>>> 19301861326ad291d1f67a7ca0e2eff49fe85704
-    pass
