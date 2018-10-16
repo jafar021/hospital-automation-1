bf5fae069e52a0d8e801fd94d4333b27f9f32fd8
Jyotirmoy Mandal <subho1301@gmail.com>
2018-10-16 16:27:19 +0530

    Updated Login page

--- hospital_automation/views.py
+++ hospital_automation/views.py
@@ -18,9 +18,13 @@ counter = 1
 
 @login_required
 def doctors(request):
-    patient = list(Patient.objects.values().filter(is_seen=False))
-    print(patient)
-    return render(request, 'incoming_patient.html', {'incoming_patient': patient})
+    user_group = request.user.groups.values_list('name', flat=True)[0]
+    if user_group == 'Doctor':
+        patient = list(Patient.objects.values().filter(is_seen=False))
+        print(patient)
+        return render(request, 'incoming_patient.html', {'incoming_patient': patient})
+    else:
+        return render(request, 'index.html', {'user_group': 'Doctor'})
 
 
 def receive_patient(request):
@@ -40,28 +44,32 @@ def receive_patient(request):
 
 @login_required
 def reception(request):
-    global counter
-    if request.method == 'GET':
-        return render(request, 'reception.html', {})
-    elif request.method == 'POST':
-        first_name = request.POST['First Name']
-        last_name = request.POST['Last Name']
-        father_name = request.POST['Father name']
-        address = request.POST['Address']
-        city = request.POST['City']
-        state = request.POST['State']
-        zip_code = request.POST['Postal/ZIP code']
-        country = request.POST['Country']
-        country_code = request.POST['country code']
-        contact_number = request.POST['Contact Number']
-        date = request.POST['todays date']
-        problem = request.POST['problem']
-        alloted_doctor = request.POST['Doctor name']
-        Patient.objects.create(first_name=first_name, last_name=last_name, guardian_name=father_name, address=address,
-                               city=city, state=state, zip_code=zip_code, country=country,
-                               country_code=country_code, phone_number=contact_number, date=date, problem_name=problem, assigned_doctor=alloted_doctor)
-        counter = 0
-        return redirect('reception')
+    user_group = request.user.groups.values_list('name', flat=True)[0]
+    if(user_group == 'Receptionist'):
+        global counter
+        if request.method == 'GET':
+            return render(request, 'reception.html', {})
+        elif request.method == 'POST':
+            first_name = request.POST['First Name']
+            last_name = request.POST['Last Name']
+            father_name = request.POST['Father name']
+            address = request.POST['Address']
+            city = request.POST['City']
+            state = request.POST['State']
+            zip_code = request.POST['Postal/ZIP code']
+            country = request.POST['Country']
+            country_code = request.POST['country code']
+            contact_number = request.POST['Contact Number']
+            date = request.POST['todays date']
+            problem = request.POST['problem']
+            alloted_doctor = request.POST['Doctor name']
+            Patient.objects.create(first_name=first_name, last_name=last_name, guardian_name=father_name, address=address,
+                                   city=city, state=state, zip_code=zip_code, country=country,
+                                   country_code=country_code, phone_number=contact_number, date=date, problem_name=problem, assigned_doctor=alloted_doctor)
+            counter = 0
+            return redirect('reception')
+    else:
+        return render(request, 'index.html', {'user_group': 'Receptionist'})
 
 
 @csrf_exempt
